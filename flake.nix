bf0c6b196627b3bcad51536a3491332193698c44
Karl Lundstig <karl@lundstig.com>
2026-03-24 12:07:35 +0100

    feat: scaffold cargo workspace, nix flake, and crate stubs

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "todomocop - personal TODO management with MCP";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+          };
+          rustToolchain = pkgs.rust-bin.stable.latest.default;
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = [
+              rustToolchain
+              pkgs.pkg-config
+            ];
+            buildInputs = [
+              pkgs.sqlite
+            ];
+          };
+        }
+      );
+    };
+}
